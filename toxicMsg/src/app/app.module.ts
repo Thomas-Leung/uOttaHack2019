50914c9bcccc9f198d67bfc4b5275dbd24198f51
David Hew-wing <dhew67@gmail.com>
2019-02-09 13:35:01 -0500

    added necessary components

--- toxicMsg/src/app/app.module.ts
+++ toxicMsg/src/app/app.module.ts
@@ -4,10 +4,18 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MessageComponent } from './message/message.component';
+import { BoardComponent } from './board/board.component';
+import { SentMessageComponent } from './board/sent-message/sent-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MessageComponent,
+    BoardComponent,
+    SentMessageComponent
   ],
   imports: [
     BrowserModule,
---
+++ toxicMsg/src/app/board/sent-message/sent-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sent-message',
+  templateUrl: './sent-message.component.html',
+  styleUrls: ['./sent-message.component.css']
+})
+export class SentMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
