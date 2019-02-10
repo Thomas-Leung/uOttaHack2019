86247a90d24e8abc7c45b176f83edf0816270bb7
thomas <thomaselmoleung@gmail.com>
2019-02-10 11:27:37 -0500

    final

--- chat/server.js
+++ chat/server.js
@@ -37,19 +37,66 @@ io.sockets.on('connection', function (socket) {
         // No BULLY
         const Url = 'http://35.237.196.72/message';
         const message = data
+
         axios({
                 method: 'post',
                 url: Url,
-                data: { message }
+                data: {
+                    message
+                }
             })
 
-            .then(response => console.log(response.data))
+            .then(response => {
+                msg = response.data
+                // strip data
+                const payloads = msg.split('payload');
+                const toxicList = [];
+                const nonToxicList = [];
+                for (const payload of payloads) {
+                    if (payload === '[') {
+                        continue;
+                    }
+                    const score = payload.substring(payload.indexOf('score'), payload.indexOf('}')).split(' ')[1].replace(/(\r\n|\n|\r)/gm, "");
+                    const display = payload.substring(payload.indexOf('display_name')).split('}')[0].split(' ')[1].replace(/(\r\n|\n|\r)/gm, "");
+                    const valid = display.localeCompare('"Toxic"');
+                    if (valid == 0) {
+                        toxicList.push(score);
+                    } else {
+                        nonToxicList.push(score);
+                    }
+                }
+                
+                var nonToxicRate = 0;
+                var toxicRate = 0;
+                for (var i = 0; i < nonToxicList.length; i++) {
+                        nonToxicRate += parseFloat(nonToxicList[i]);
+                }
+                for (var i = 0; i < toxicList.length; i++) {
+                        toxicRate += parseFloat(toxicList[i]);
+                }
+                console.log(nonToxicRate);
+                console.log(toxicRate);
+                if (nonToxicRate >= toxicRate) {
+                    io.sockets.emit("new message", {
+                        msg: data,
+                        user: socket.username,
+                        toxic: "false"
+                    });
+                } else {
+                    // console.log(data);
+                    io.sockets.emit("new message", {
+                        msg: data,
+                        user: socket.username,
+                        toxic: "true"
+                    });
+                }
+                // console.log(msg);
+                // console.log(nonToxicList);
+                // console.log(toxicList);
+            })
             .catch(err => console.log(err))
-        // console.log(data);
-        io.sockets.emit("new message", {
-            msg: data,
-            user: socket.username
-        });
+
+
     });
 
     // New User 
