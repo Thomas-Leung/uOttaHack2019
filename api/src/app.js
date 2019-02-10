7d42f013fa6409b6c6b1cf82ab615d70c4366f3c
Derek Shao <shao.derek.now@gmail.com>
2019-02-10 05:47:35 -0500

    entrypoint

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,26 @@
 const express = require("express")
-
+const http = require("http")
+const bodyParser = require("body-parser")
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post("/message", (req, res) => {
+    
+    message = req.body.message
+
+    http.get('http://127.0.0.1:8081/toxicity', (resp) => {
+        let data = ''
+
+        resp.on('data', (chunk) => {
+            data += chunk
+        })
+
+        resp.on('end', () => {
+            console.log(JSON.parse(data).explanation);
+        });
+    })
+})
+
+http.createServer(app).listen(80)
